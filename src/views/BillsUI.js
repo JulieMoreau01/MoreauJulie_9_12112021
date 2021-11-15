b518e17f06c55270ba20bef4c93e8bc908c75493
Julie Moreau <julie_bonnet_01@hotmail.com>
2021-11-15 16:23:34 +0100

    [Bug Report] - Login & Bills

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -19,12 +19,24 @@ const row = (bill) => {
     `)
   }
 
+// const rows = (data) => {
+//   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+// }
+
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  if (data && data.length) {
+    data.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    })
+    console.log(data)
+    return data.map(bill => row(bill)).join("")
+  } else {
+    return ""
+  }
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
